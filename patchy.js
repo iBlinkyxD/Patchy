cd808393db50cdea52998d3e3dc482cca8899c6f
iBlinkyxD <ibluntyyt@gmail.com>
2021-10-28 11:38:38 -0400

    Patchy 2.0.0 - Alpha Testing
    Added "Guild_Voice_States"

--- patchy.js
+++ patchy.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION", "GUILD_VOICE_STATES"]});
 const { CommandHandler } = require('djs-commands');
 const distube = require('distube');
 
